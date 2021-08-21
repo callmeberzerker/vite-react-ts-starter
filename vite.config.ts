338cc5fb81971e8858f61f9bc67232d94bccf406
Spase Markovski <smarkovski@gmail.com>
2021-08-21 21:05:17 +0200

    upgrade deps

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 import path from 'path'
 import { defineConfig } from 'vite'
 import macrosPlugin from 'vite-plugin-babel-macros'
+import notifier from 'vite-plugin-notifier'
 import reactJsx from 'vite-react-jsx'
 const projectRootDir = path.resolve(__dirname)
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [macrosPlugin(), reactJsx(), reactRefresh(), graphqlPlugin()],
+  plugins: [macrosPlugin(), reactJsx(), reactRefresh(), graphqlPlugin(), notifier()],
   resolve: {
     alias: [{ find: 'src', replacement: path.resolve(projectRootDir, 'src') }],
   },
